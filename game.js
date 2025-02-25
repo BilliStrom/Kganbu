639097205920917fc0617bd517c03e2c05fd2d03
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-02-26 01:53:24 +0300

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ class FlappyGame {
         this.highscoreDisplay = document.getElementById('highscore');
         
         this.gravity = 0.3;
-        this.jumpForce = -0.3;
+        this.jumpForce = -15;
         this.pipeSpeed = 1;
         this.score = 0;
         this.highscore = 0;
