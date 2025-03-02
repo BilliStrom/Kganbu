b83655df2272fea39833041cfc67e55708f0e980
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-03-02 06:37:41 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-
+// Конфигурация Firebase
 const firebaseConfig = {
             apiKey: "AIzaSyA7IL_lkuJ7klzKFJCwFjci7eOW-aLQrUw",
             authDomain: "knb-clicker-game.firebaseapp.com",
@@ -11,8 +8,7 @@ const firebaseConfig = {
             appId: "1:810664187137:web:b53c6e6ba9bfbadc6c7700"
         };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db, signInWithEmailAndPassword, signOut, collection, addDoc, getDocs };
+// Инициализация Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
